079d675b6d0f7a7fe25dbe5bc11d66e8da42f584
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-03-15 01:55:05 +0700

    add policy enity type

--- src/types/entity.ts
+++ src/types/entity.ts
@@ -4,6 +4,7 @@ export enum CDEntityType {
   FTF_LOCATION = "ftf-location",
   FTF_DEPARTMENT = "ftf-department",
   FTF_TENANT = "ftf-tenant",
+  FTF_PRIVACY_POLICY = "ftf-privacyPolicy",
 }
 
 export interface CDEntity {

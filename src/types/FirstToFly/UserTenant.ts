edd361823f02bddaaf81793c5f05879e5582d3e3
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-03-23 03:06:05 +0700

    add comment

--- src/types/FirstToFly/UserTenant.ts
+++ src/types/FirstToFly/UserTenant.ts
@@ -4,6 +4,9 @@ import { CDEntity } from "../entity";
  * @export
  * @interface FTFUserTenant
  * @extends {CDBaseEntity}
+ * @description A composite entity that holds the user's tenant specific data.
+ * Its oid is the composite of the user's oid and the tenant's oid.
+ * OID Structure: ftf-userTenant-[userOID]|[tenantOID]
  */
 export interface FTFUserTenant extends CDEntity {
   tenantOID: string;

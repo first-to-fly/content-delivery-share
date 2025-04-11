121a5cdde610defe4535e152d7604b17373dfc6c
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-04-12 00:45:37 +0700

    finalize GroupTourProduct

--- src/types/FirstToFly/GroupTourProduct.ts
+++ src/types/FirstToFly/GroupTourProduct.ts
@@ -7,22 +7,35 @@ import { MultiLangRecord } from "../multipleLanguage";
  * @extends {CDEntity}
  */
 export interface FTFGroupTourProduct extends CDEntity {
-  productCode: string;
+  code: string;
+
   name: MultiLangRecord<string>;
   description: MultiLangRecord<string> | null;
+
   departmentOID: string;
+  sectorOIDs: string[];
+  displaySectorOIDs: string[];
+
+  sectorGroupOID: string | null;
+  itineraryOIDs: string[] | null;
+  costingOIDs: string[] | null;
+
   shoutout: MultiLangRecord<string> | null;
   writeup: MultiLangRecord<string> | null;
   highlights: MultiLangRecord<string> | null;
   importantNotes: MultiLangRecord<string> | null;
   inclusions: MultiLangRecord<string> | null;
   exclusions: MultiLangRecord<string> | null;
-  durationDays: number | null;
-  durationNights: number | null;
-  validityStartDate: string | null;
-  validityEndDate: string | null;
-  salesPeriodStartDate: string | null;
-  salesPeriodEndDate: string | null;
+
+  durationDays: number;
+  durationNights: number;
+
+  validityStartDate: string;
+  validityEndDate: string;
+
+  salesPeriodStartDate: string;
+  salesPeriodEndDate: string;
+
   isActive: boolean;
   published: boolean;
 

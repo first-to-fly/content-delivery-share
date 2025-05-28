30c1c4f54c37c4724f71c1f796ea6cefd1e15d7c
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-05-28 18:37:32 +0700

    add support for converting objects from null to undefined keys

--- src/utils.ts
+++ src/utils.ts
@@ -7,12 +7,77 @@ import { CDEntity, CDEntityType } from "./types/entity";
 import { OID } from "./types/generic";
 
 
+// Deliberately not support deep convert nullable to optional,
+// since this is used for converting the first level of the nullable keys from CD
 export type NullableToOptional<T> = {
   [P in keyof T as null extends T[P] ? never : P]: T[P];
 } & {
   [P in keyof T as null extends T[P] ? P : never]?: Exclude<T[P], null>;
 };
 
+export const nullToUndefined = <T>(obj: T): NullableToOptional<T> => {
+  if (obj === null || typeof obj !== "object") {
+    return obj as NullableToOptional<T>;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((item) => nullToUndefined(item)) as unknown as NullableToOptional<T>;
+  }
+
+  const result = { ...obj } as Record<string, unknown>;
+  // Only support the first level of the nullable keys from CD,
+  // this is to keep deeper structure of the object
+  // If you want to convert the deeper structure, you can use deepNullToUndefined
+  for (const key in result) {
+    if (Object.prototype.hasOwnProperty.call(result, key)) {
+      if (result[key] === null) {
+        result[key] = undefined;
+      }
+    }
+  }
+  return result as NullableToOptional<T>;
+};
+
+
+type DeepNullToUndefined<T> = T extends Primitive | Date
+  ? T
+  : T extends (infer U)[]
+    ? DeepNullToUndefined<U>[]
+    : T extends readonly (infer U)[]
+      ? readonly DeepNullToUndefined<U>[]
+      : {
+        [P in keyof T]: null extends T[P]
+          ? Exclude<T[P], null> extends object | undefined
+            ? DeepNullToUndefined<Exclude<T[P], null>> | undefined
+            : Exclude<T[P], null> | undefined
+          : T[P] extends object | undefined
+            ? DeepNullToUndefined<T[P]>
+            : T[P];
+      };
+
+type Primitive = string | number | boolean | bigint | symbol | undefined | null;
+
+export function deepNullToUndefined<T>(obj: T): DeepNullToUndefined<T> {
+  if (obj === null || typeof obj !== "object") {
+    return obj as DeepNullToUndefined<T>;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((item) => deepNullToUndefined(item)) as DeepNullToUndefined<T>;
+  }
+
+  const result = { ...obj } as Record<string, unknown>;
+  for (const key in result) {
+    if (Object.prototype.hasOwnProperty.call(result, key)) {
+      if (result[key] === null) {
+        result[key] = undefined;
+      } else if (typeof result[key] === "object") {
+        result[key] = deepNullToUndefined(result[key]);
+      }
+    }
+  }
+  return result as DeepNullToUndefined<T>;
+}
 
 const checkIsOIDRegex = new RegExp(`^(${Object.values(CDEntityType).map((t) => t.replaceAll("-", "\\-")).join("|")})\\-`, "");
 

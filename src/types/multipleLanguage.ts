50968efb6b6f8e00e839e235dace1f11aa3956c9
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-05-29 11:53:37 +0700

    add support getting multi lang record given preferred langs

--- src/types/multipleLanguage.ts
+++ src/types/multipleLanguage.ts
@@ -4,3 +4,16 @@ import { LanguageCode } from "./enums/language";
 export type MultiLangRecord<T> = {
   [key in LanguageCode]?: T
 };
+
+export function getMultiLangRecord<T>(record: MultiLangRecord<T>, preferredLanguages?: LanguageCode[]): T | undefined {
+  for (let i = 0; i < (preferredLanguages?.length ?? 0); i++) {
+    const language = preferredLanguages?.[i];
+    if (language && record[language]) {
+      return record[language];
+    }
+  }
+
+  const firstLanguage = Object.keys(record)
+    .sort((a, b) => a.localeCompare(b))[0] as LanguageCode;
+  return record[firstLanguage];
+}

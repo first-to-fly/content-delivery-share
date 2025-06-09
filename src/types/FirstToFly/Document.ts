9e0df0080a974e3786437cc82d2c8b82c3ea7357
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-06-09 16:53:07 +0700

    add DocumentType enum to FTFDocument for improved type safety

--- src/types/FirstToFly/Document.ts
+++ src/types/FirstToFly/Document.ts
@@ -1,6 +1,18 @@
 import { CDEntity } from "../entity";
 import { NamedURL } from "../url";
 
+
+export enum DocumentType {
+  PASSPORT = "passport",
+  NATIONAL_ID = "national_id",
+  DRIVING_LICENSE = "driving_license",
+  VISA = "visa",
+  MARRIAGE_CERTIFICATE = "marriage_certificate",
+  BIRTH_CERTIFICATE = "birth_certificate",
+  TRAVEL_PERMIT = "travel_permit",
+  OTHER = "other",
+}
+
 /**
  * @export
  * @interface FTFDocument
@@ -10,7 +22,7 @@ export interface FTFDocument extends CDEntity {
 
   entityOID: string; // linkage to the entity that owns this document
 
-  type: string;
+  type: DocumentType;
   name: string;
   docIdentification: string;
 

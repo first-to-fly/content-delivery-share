0d95e46f6a995a8e0218b2671935077edee2debb
Phan Tran Le Nguyen <nguyen@inkr.com>
2025-08-09 15:27:56 +0700

    add account code

---
+++ src/types/FirstToFly/AccountCode.ts
@@ -0,0 +1,33 @@
+import { CDEntity } from "../entity";
+
+/**
+ * @export
+ * @interface FTFAccountCode
+ * @extends {CDEntity}
+ */
+export interface FTFAccountCode extends CDEntity {
+  tenantOID: string;
+
+  code: string;
+  name: string;
+  description: string | null;
+  type: string;
+  status: string;
+  currency: string | null;
+
+  parentAccountCodeOID: string | null;
+
+  xeroAccountId: string | null;
+  xeroAccountCode: string | null;
+  xeroSyncStatus: string | null;
+  xeroSyncedAt: string | null;
+
+  isSystemGenerated: boolean;
+  allowManualJournals: boolean;
+  taxType: string | null;
+
+  createdAt: string;
+  updatedAt: string;
+  createdBy: string;
+  updatedBy: string | null;
+}
